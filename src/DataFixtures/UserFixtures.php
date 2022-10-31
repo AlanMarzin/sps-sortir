2ebf2ca0e20ce71c326e7c6c398784eaecd2fa74
amarzin2022 <alan.marzin2022@campus-eni.fr>
2022-10-31 17:00:36 +0100

    formulaire de login preparation du logout config securite csrf dans security.yaml composer require orm-fixtures --dev creation de UserFixtures et de trois utilisateurs alan,fred,axelle hasher les mots de passe

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        $axelle = new User();
+        $axelle->setPseudo('axelle');
+        $axelle->setEmail('axelle.cardin@gmail.com');
+        $axelle->setPassword($this->hasher->hashPassword($axelle, 'axelle'));
+        $manager->persist($axelle);
+
+        $fred = new User();
+        $fred->setPseudo('fred');
+        $fred->setEmail('fred.arthaud@gmail.com');
+        $fred->setPassword($this->hasher->hashPassword($fred, 'fred'));
+        $manager->persist($fred);
+
+        $alan = new User();
+        $alan->setPseudo('alan');
+        $alan->setEmail('alan.marzin@gmail.com');
+        $alan->setPassword($this->hasher->hashPassword($alan, 'alan'));
+        $manager->persist($alan);
+
+
+        $manager->flush();
+
+    }
+}
