af00aee3508f3cfe84b388c0471210471892acb6
axelle <cardin.axelle@live.fr>
2022-11-01 18:50:52 +0100

    c'est réparé

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SortieController extends AbstractController
 {
-    #[Route('/', name: 'sortie_list')]
+    #[Route('/', name: 'app_login')]
     public function list(AuthenticationUtils $authentificationUtils, SortieRepository $sortieRepository, CampusRepository $campusRepository): Response
     {
         //obtenir le login error s'il y en a un
