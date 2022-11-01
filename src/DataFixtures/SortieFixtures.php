58ff7e5962b6c9c304de1d8d1193c4b0fe6516d2
axelle <cardin.axelle@live.fr>
2022-11-01 13:12:45 +0100

    màj des classes Etat et Sortie + création des fixtures EtatFixtures et SortieFixtures

---
+++ src/DataFixtures/SortieFixtures.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Sortie;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class SortieFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $escalade = new Sortie();
+        $escalade->setNom('Grimpette au Roof');
+        $escalade->setDateHeureDebut(DateTime::createFromFormat('d-m-y H:i:s', '02-11-22 17:30:00'));
+        $escalade->setDateLimiteInscription(DateTime::createFromFormat('d-m-y', '31-10-22'));
+        $escalade->setDuree(180);
+        $escalade->setInfosSortie('Session grimpe puis session bière.');
+        $escalade->setNbInscriptionsMax(10);
+        $escalade->setEtat($this->getReference('etat-terminee'));
+        $manager->persist($escalade);
+
+        $muscu = new Sortie();
+        $muscu->setNom('Muscu chez Fitness Park');
+        $muscu->setDateHeureDebut(DateTime::createFromFormat('d-m-y H:i:s', '22-11-22 18:00:00'));
+        $muscu->setDateLimiteInscription(DateTime::createFromFormat('d-m-y', '20-11-22'));
+        $muscu->setDuree(90);
+        $muscu->setInfosSortie('Concours de muscle up chez Fitness Park');
+        $muscu->setNbInscriptionsMax(3);
+        $muscu->setEtat($this->getReference('etat-creation'));
+        $manager->persist($muscu);
+
+        $moto = new Sortie();
+        $moto->setNom('Balade en moto');
+        $moto->setDateHeureDebut(DateTime::createFromFormat('d-m-y H:i:s', '10-11-22 12:30:00'));
+        $moto->setDateLimiteInscription(DateTime::createFromFormat('d-m-y', '08-11-22'));
+        $moto->setDuree(60);
+        $moto->setInfosSortie('Balade en moto à l\'heure du déjeuner.');
+        $moto->setNbInscriptionsMax(6);
+        $moto->setEtat($this->getReference('etat-ouverte'));
+        $manager->persist($moto);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [EtatFixtures::class];
+    }
+
+}
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -16,26 +17,27 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        $axelle = new User();
-        $axelle->setPseudo('axelle');
-        $axelle->setEmail('axelle.cardin@gmail.com');
-        $axelle->setPassword($this->hasher->hashPassword($axelle, 'axelle'));
-        $manager->persist($axelle);
-
-        $fred = new User();
-        $fred->setPseudo('fred');
-        $fred->setEmail('fred.arthaud@gmail.com');
-        $fred->setPassword($this->hasher->hashPassword($fred, 'fred'));
-        $manager->persist($fred);
-
-        $alan = new User();
-        $alan->setPseudo('alan');
-        $alan->setEmail('alan.marzin@gmail.com');
-        $alan->setPassword($this->hasher->hashPassword($alan, 'alan'));
-        $manager->persist($alan);
-
-
-        $manager->flush();
+//        $axelle = new User();
+//        $axelle->setPseudo('axelle');
+//        $axelle->setEmail('axelle.cardin@gmail.com');
+//        $axelle->setPassword($this->hasher->hashPassword($axelle, 'axelle'));
+//        $manager->persist($axelle);
+//
+//        $fred = new User();
+//        $fred->setPseudo('fred');
+//        $fred->setEmail('fred.arthaud@gmail.com');
+//        $fred->setPassword($this->hasher->hashPassword($fred, 'fred'));
+//        $manager->persist($fred);
+//
+//        $alan = new User();
+//        $alan->setPseudo('alan');
+//        $alan->setEmail('alan.marzin@gmail.com');
+//        $alan->setPassword($this->hasher->hashPassword($alan, 'alan'));
+//        $manager->persist($alan);
+//
+//
+//        $manager->flush();
 
     }
+
 }
