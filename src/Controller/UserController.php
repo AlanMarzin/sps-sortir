caeddcce3bf1aa6d80f90efcdab03b4e7847f331
Akila061 <arthaud.frederic@gmail.com>
2022-11-02 12:24:50 +0100

    * Possiblité de modifier un profil sans l'obligation de rentrer le mot de passe. * Mise à jour de la base de donnée OK

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\ProfileTypeForm;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,15 +18,28 @@ class UserController extends AbstractController
     public function detail(Request $request, int $id, UserRepository $userRepository): Response
     {
         $user = $userRepository->find($id);
-        $profileForm = $this->createForm(ProfileTypeForm::class, $user);
-        $profileForm->handleRequest($request);
-
 
         if ($user === null) {
             throw $this->createNotFoundException('Page not found');
         }
 
         return $this->render('profile/gestionprofile.html.twig', [
+            'id' => $id,
+            'user' => $user,
+        ]);
+    }
+    #[Route('update/{id}', name: 'profil_update', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function update(Request $request, int $id, UserRepository $userRepository, EntityManagerInterface $em, User $user): Response{
+            $user = $userRepository->find($id);
+            $profileForm = $this->createForm(ProfileTypeForm::class, $user,['validation_groups' => ['Default'],]);
+            $profileForm->handleRequest($request);
+
+            if (($profileForm->isSubmitted())){
+                $em->flush();
+                $this->addFlash('success', 'Votre profil a été mis à jour');
+            }
+
+        return $this->render('profile/gestionprofile.html.twig',[
             'id' => $id,
             'user' => $user,
             'profileForm' => $profileForm->createView()
--- src/Form/ProfileTypeForm.php
+++ src/Form/ProfileTypeForm.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -15,12 +17,37 @@ class ProfileTypeForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            //Pour la gestion des Photos de profil
+//            ->add('photoProfil', FileType::class, [
+//                'mapped' => true,
+//                'label' => 'Modifier votre Photo : '
+//            ])
             ->add('email')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('pseudo')
-            ->add('telephone')
+            ->add('password', PasswordType::class, [
+                'label'=> 'Mot de Passe : ',
+                'required'=>false,
+                'mapped' => false,
+                'attr' =>['autocomplete'=>'nouveau mot de passe'],
+                'constraints'=>[
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Votre mot de passe doit être de 3 caractères minimun',
+                        'max' => 4096,
+                    ]),
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'label'=>'Nom : '
+            ])
+            ->add('prenom', TextType::class, [
+                'label'=>'Prénom : '
+            ])
+            ->add('pseudo', TextType::class, [
+                'label'=>'Pseudo : '
+            ])
+            ->add('telephone', TextType::class, [
+                'label'=>'Numéro de téléphone : '
+            ])
         ;
     }
 
