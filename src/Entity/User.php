e5569add0b9f2e7b85ef0e9678218e75eb82e203
AlanMarzin <alan.marzin2022@campus-eni.fr>
2022-11-02 11:57:15 +0100

    Modification de UserRepository pour se logger avec mail ET pseudo ! VICTOIRE <3

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,7 +35,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     private ?string $prenom = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $pseudo = null;
 
     #[ORM\Column(length: 255)]
