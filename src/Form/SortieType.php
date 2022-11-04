6519e406e72730f580f719360fe80e1815ffb129
Akila061 <arthaud.frederic@gmail.com>
2022-11-04 14:58:02 +0100

    * Modification de SortieType.php en mettant un "IntegerType" pour définir le type Form de la durée.
    * Affichage du campus du créateur d'une sortie dans le select des campus.

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -6,19 +6,28 @@ use App\Entity\Campus;
 use App\Entity\Etat;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class SortieType extends AbstractType
 {
+    private $security;
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -29,7 +38,7 @@ class SortieType extends AbstractType
                 'label'=>'Date de début : ',
                 'widget'=>'single_text'
             ])
-            ->add('duree', NumberType::class, [
+            ->add('duree', IntegerType::class, [
                 'label'=>'Durée de l\'activité : '
             ])
             ->add('dateLimiteInscription', DateType::class, [
@@ -40,21 +49,34 @@ class SortieType extends AbstractType
             ->add('nbInscriptionsMax', NumberType::class, [
                 'label'=>'Nombre de participants MAX : '
             ])
+
             ->add('infosSortie', TextareaType::class, [
-                    'label'=>'Description : '
+                'label'=>'Description : '
+
             ])
 
             ->add('lieu', EntityType::class, [
                 'class'=>Lieu::class,
                 'choice_label'=>'nom',
-                'label'=>'Lieu : '
+                'label'=>'Lieu : ',
             ])
 
-             ->add('campus', EntityType::class, [
-                 'class'=>Campus::class,
-                 'choice_label'=>'nom',
-                 'label'=>'Campus : '
-             ])
+//             ->add('campus', EntityType::class, [
+//                 'class'=>Campus::class,
+//                 'choice_label'=>'nom',
+//                 'label'=>'Campus : '
+//             ])
+
+            ->add('campus', EntityType::class, [
+                'class'=>Campus::class,
+//                'query_builder'=> function (EntityRepository $er){
+//                return $er->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
+//                },
+                'choice_label'=>'nom',
+                'label'=>'Campus : ',
+                'data'=> $this->security->getUser()->getCampus()
+
+            ])
         ;
     }
 
